7dd92ac95dc61fb55e6bb6fc9808c379f6e07fb7
CraigB <sbranscom2@radford.edu>
2019-02-23 17:55:36 -0800

    build script

---
+++ contracts/build.sh
@@ -0,0 +1,8 @@
+# -contract=<string>       - Contract name
+# -o=<string>              - Write output to <file>
+# -abigen                  - Generate ABI
+# -I=<string>              - Add directory to include search path
+# -L=<string>              - Add directory to library search path
+
+#eosio.cdt v1.5.0
+eosio-cpp -I="./include/" -o="grassroots.wasm" -contract="grassroots" -abigen /grassroots/src/grassroots.cpp
